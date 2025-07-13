7579c92b69dd132744baa522b38b10fadc6027a0
zeonstha <57930213+zeonstha@users.noreply.github.com>
2025-07-13 10:29:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -86,6 +86,7 @@ resource "aws_security_group" "web-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
